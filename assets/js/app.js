9fb3a96a7c65b4a3500f584c1a4727b8b264a7dd
Steve Signell <steve@frontierspatial.com>
2014-11-18 11:46:41 -0500

    removed grouped layer control

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,7 +53,7 @@ $("#sidebar-hide-btn").click(function() {
 function sidebarClick(id) {
   map.addLayer(birdLayer).addLayer(amphibianLayer);
   var layer = markerClusters.getLayer(id);
-  map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 18);
+  map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 16);
   layer.fire("click");
   /* Hide sidebar and go to the map on small screens */
   if (document.body.clientWidth <= 767) {
@@ -229,7 +229,8 @@ $.getJSON("data/observationamphibian.geojson", function (data) {
 
 map = L.map("map", {
   zoom: 10,
-  center: [40.702222, -73.979378],
+  maxZoom: 16,
+  //center: [40.702222, -73.979378],
   layers: [mapquestHYB, site, markerClusters, highlight],
   zoomControl: false,
   attributionControl: false
@@ -329,17 +330,13 @@ var baseLayers = {
   "Imagery with Streets": mapquestHYB
 };
 
-var groupedOverlays = {
-  "Points of Interest": {
+var overlays = {
     "<img src='assets/img/bird.png' width='24' height='28'>&nbsp;Bird Surveys": birdLayer,
-    "<img src='assets/img/amphibian.png' width='24' height='28'>&nbsp;Amphibian Surveys": amphibianLayer
-  },
-  "Reference": {
-    "Survey Sites": site
-  }
+    "<img src='assets/img/amphibian.png' width='24' height='28'>&nbsp;Amphibian Surveys": amphibianLayer,
+    "<img src='assets/img/site.png' width='24' height='28'>&nbsp;Survey Sites": site
 };
 
-var layerControl = L.control.groupedLayers(baseLayers, groupedOverlays, {
+var layerControl = L.control.layers(baseLayers, overlays, {
   collapsed: isCollapsed
 }).addTo(map);
 
